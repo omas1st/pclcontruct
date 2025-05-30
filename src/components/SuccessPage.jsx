a86dfd370b1a1800c0767ab0b05b0d2779218432
omas1st <omas1st@gmail.com>
2025-05-30 15:26:14 +0100

    Update SuccessPage.jsx

--- src/components/SuccessPage.jsx
+++ src/components/SuccessPage.jsx
@@ -6,8 +6,8 @@ const SuccessPage = () => (
     <div className="success-message">
       <h1>Thank You!</h1>
       <p>Your application has been submitted successfully.</p>
-      <p>Our management team will reach out to you at the email address you provided.</p>
       <p>Submit your CV/resume to americanengineringgroups@gmail.com today.</p>
+      <p>Our management team will reach out to you at the email address you provided.</p>
     </div>
   </div>
 );
